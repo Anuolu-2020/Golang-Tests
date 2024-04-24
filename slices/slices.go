ff8eea5277167559a600ecfaf8682e1003952bf1
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-24 23:57:48 +0100

    feat: implements slices tests

---
+++ slices/slices.go
@@ -0,0 +1,21 @@
+package slices
+
+func Sum(numbers []int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, number := range numbersToSum {
+		sums = append(sums, Sum(number))
+	}
+
+	return sums
+}
---
+++ slices/slices_test.go
@@ -0,0 +1,28 @@
+package slices
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("Collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("Got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("Got %d want %d ", got, want)
+	}
+}
